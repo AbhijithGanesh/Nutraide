752d0c4e5d3bedfc85a1ec85db9175b7c8281ef7
AbhijithGanesh <git.watch.Abhijith@outlook.com>
2023-04-15 12:09:14 +0530

    Addition of new values
    Signed-off-by: AbhijithGanesh <git.watch.Abhijith@outlook.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AiFillGithub } from "react-icons/ai"
 import { CiLogin } from "react-icons/ci"
 import { TbApi } from "react-icons/tb"
+import { GiHealthPotion } from "react-icons/gi"
 
 const ComingSoonPage: React.FC = () => {
   const currentDate = new Date();
@@ -10,11 +11,11 @@ const ComingSoonPage: React.FC = () => {
       <div className="max-w-lg mx-auto p-8 bg-gray-800 rounded-lg shadow-xl">
         <h1 className="text-4xl font-black mb-4 text-gray-100">Nutraide App</h1>
         <p className="text-gray-300 text-xl mb-8">
-          We're working to make this App to come to life! Stay tuned 🚀🌟 
+          We're working to make this App to come to life! Stay tuned 🚀🌟
         </p>
         <div className="flex flex-1 space-x-4">
           <a className="rounded-full h-12 w-12 flex items-center justify-center bg-gray-700 hover:translate-y-2" href="https://github.com/AbhijithGanesh/Nutraide.git" target='__blank'>
-            <AiFillGithub className="text-white text-4xl"/>
+            <AiFillGithub className="text-white text-4xl" />
           </a>
           <a className="rounded-full h-12 w-12 flex items-center justify-center bg-gray-700 hover:translate-y-2" href="/login">
             <CiLogin className="text-white text-4xl" />
@@ -22,11 +23,14 @@ const ComingSoonPage: React.FC = () => {
           <div className="rounded-full h-12 w-12 flex items-center justify-center bg-gray-700 hover:translate-y-2">
             <TbApi className="text-white text-4xl" />
           </div>
-        </div>
-        <p className="text-gray-300 text-sm mt-8">&copy; {currentDate.getFullYear()} Team Squiggly Kraken.</p>
-        <p className="text-gray-300 text-xs my-0">Abhijith G, Jesher Joshua, Jaskaran Walia and Sree Dananjay</p>
+          <a className="rounded-full h-12 w-12 flex items-center justify-center bg-gray-700 hover:translate-y-2" href="https://nutriai-url.vercel.app/" target='__blank'>
+            <GiHealthPotion className="text-white text-4xl" />
+        </a>
       </div>
+      <p className="text-gray-300 text-sm mt-8">&copy; {currentDate.getFullYear()} Team Squiggly Kraken.</p>
+      <p className="text-gray-300 text-xs my-0">Abhijith G, Jesher Joshua, Jaskaran Walia and Sree Dananjay</p>
     </div>
+    </div >
   );
 };
 
