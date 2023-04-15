8bb9d3412ccad3b3cfff5c971933f67493729f31
AbhijithGanesh <git.watch.Abhijith@outlook.com>
2023-04-15 12:15:29 +0530

    Addition of links
    Signed-off-by: AbhijithGanesh <git.watch.Abhijith@outlook.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,8 @@ const ComingSoonPage: React.FC = () => {
         <h1 className="text-4xl font-black mb-4 text-gray-100">Nutraide App</h1>
         <p className="text-gray-300 text-xl mb-8">
           We're working to make this App to come to life! Stay tuned 🚀🌟
-          Click on the health potion to try the calculator out!
+          <br/>
+          <section className="text-sm">Click on the health potion to try the calculator out!</section>
         </p>
         <div className="flex flex-1 space-x-4">
           <a className="rounded-full h-12 w-12 flex items-center justify-center bg-gray-700 hover:translate-y-2" href="https://github.com/AbhijithGanesh/Nutraide.git" target='__blank'>
