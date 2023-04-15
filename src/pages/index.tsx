5561139b06d16adc346abb83bb05897c504889b3
AbhijithGanesh <git.watch.Abhijith@outlook.com>
2023-04-15 10:50:08 +0530

    New brand name🌟
    Signed-off-by: AbhijithGanesh <git.watch.Abhijith@outlook.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const ComingSoonPage: React.FC = () => {
       <div className="max-w-lg mx-auto p-8 bg-gray-800 rounded-lg shadow-xl">
         <h1 className="text-4xl font-black mb-4 text-gray-100">NutriLicious App</h1>
         <p className="text-gray-300 text-xl mb-8">
-          We're working to make this App to come to life! Stay tuned!
+          We're working to make this App to come to life! Stay tuned 🚀🌟 
         </p>
         <div className="flex flex-1 space-x-4">
           <div className="rounded-full h-12 w-12 flex items-center justify-center bg-gray-700 hover:translate-y-2">
